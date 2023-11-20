dbce121a896936c33dab83ee5f2644d1d184a2d1
Iradukunda Bonte <irabonte123@gmail.com>
2023-11-20 12:47:59 -0800

    developing APIs

---
+++ Bake/bake/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Bake from './components/Bake'; 
+import Table from './components/Table'; 
+
+function App() {
+  return (
+     <React.Fragment>
+        <Table />
+        <Bake />
+     </React.Fragment>
+         
+ 
+  );
+}
+
+export default App;
